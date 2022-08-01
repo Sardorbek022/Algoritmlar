21b70522e1c72bc03d5012e604d398e3280cc778
Sardor <104882911+Sardorbek022@users.noreply.github.com>
2022-08-01 15:27:17 +0500

    yangi fayl qo'shdim

---
+++ max_min.py
@@ -0,0 +1,354 @@
+# Bismillahir roxmanir roxim
+
+# # 1-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# min = a
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max < a:
+#         max = a
+#     elif min > a:
+#         min = a
+# print(f"max {max} , min {min}")
+
+# # 2-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# b = int(input("b = "))
+# s = a * b
+# min = s
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     b = int(input("b = "))
+#     s = a * b
+#     if min > s:
+#         min = s
+# print(min)
+
+# # 3-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# b = int(input("b = "))
+# p = 2 * (a + b)
+# max = p
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     b = int(input("b = "))
+#     p = 2 * (a + b)
+#     if max < p:
+#         max = p
+# print(max)
+
+# # 4-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# count = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if min > a:
+#         min = a
+#         count = i
+# print(count)
+        
+# # 5-masala:
+# n = int(input("n = "))
+# m = int(input("m = "))
+# v = int(input("v = "))
+# b = m / v
+# max = b
+# for i in range(2,n+1):
+#     m = int(input("m = "))
+#     v = int(input("v = "))
+#     b = m / v
+#     if max < b:
+#         max = b
+# print(max) 
+
+# # 6-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# min = a
+# count1 = 1 
+# count2 = 1 
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if min > a:
+#         min = a
+#         count1 = i
+#     elif max <= a:
+#         max = a
+#         count2 = i
+# print(count1)
+# print(count2)
+
+# # 7-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# min = a
+# count1 = 1
+# count2 = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max < a:
+#         max = a
+#         count1 = i
+#     elif min >= a:
+#         max = a
+#         count2 = i
+# print(count1)
+# print(count2)
+
+# # 8-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# count1 = 1
+# count2 = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if min > a:
+#         min = a
+#         count1 = i
+#     elif min >= a:
+#         min = a
+#         count2 = i
+# print(f"birinchi uchragan minning tartib raqami {count1}, oxirgis {count2}")
+
+# # 9-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# count1 = 1
+# count2 = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max < a:
+#         max = a
+#         count1 = i
+#     elif max <= a:
+#         max = a
+#         count2 = i
+# print(f"birinchi uchragan maxning tartib raqami {count1}, oxirgis {count2}")
+
+# # 10-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# max = a
+# count1 = 1
+# count2 = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max < a:
+#         max = a
+#         count1 = i
+#     elif min > a:
+#         min = a
+#         count2 = i
+# print(f"birinchi uchragan max {count1}, min {count2}")
+
+# # 11-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# max = a
+# count1 = 1
+# count2 = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max <= a:
+#         max = a
+#         count1 = i
+#     elif min >= a:
+#         min = a
+#         count2 = i
+# print(f"oxirgi uchragan max {count1}, min {count2}")
+
+# # 12-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if a > 0 and min > a:
+#         min = a
+# print(min)
+
+# # 13-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# count = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if a % 2 != 0:
+#         if max < a:
+#             max = a
+#             count = i
+# print(count)
+
+# 14-masala:
+        
+# # 16-massala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# count = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if min > a:
+#         min = a
+#         count = i
+# print(count - 1)
+
+# # 17-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# count = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max <= a:
+#         max = a
+#         count = i
+# print(n - count)
+
+# # 18-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# count0 = 1
+# count = 1
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     if max < a:
+#         max = a
+#         count0 = i
+#     elif max <= a:
+#         max = a
+#         count = i
+# print(count - count0 - 1)
+
+# # 19-masala: chala:
+# n = int(input("n = "))
+# x = []
+# y = []
+# min = 0
+# for i in range(n):
+#     s = i + 1
+#     a = int(input(f"{s}-element: "))
+#     x.append(a)
+# for j in range(len(x)):
+#     if min > x[j]:
+#         min = x[j]
+# min1 = min
+# for k in range(len(x)):
+#     if x[k] == min1:
+#         y.append(x[k])
+# print(y)
+
+# # 21-masala:
+# n = int(input("n = "))
+# a = int(input("a = "))
+# max = a
+# min = a
+# sum = 0
+# x = []
+# x.append(a)
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     x.append(a)
+#     if max < a:
+#         max = a
+#     elif min > a:
+#         min = a
+# for j in range(len(x)):
+#     sum += x[j]
+# print((sum - min - max) / (n - 2))
+
+# # 22-masala:
+# n = int(input("n = "))
+# x = []
+# y = []
+# z = []
+# for i in range(n):
+#     a = int(input(f"{i + 1}-element: "))
+#     x.append(a)
+# for j in range(len(x)):
+#     min = x[0]
+#     if min > x[j]:
+#         min = x[j]
+# for k in range(len(x)):
+#     if x[k] == min:
+#         continue
+#     y.append(x[k])
+# for l in range(len(y)):
+#     min1 = y[0]
+#     if min1 > y[l]:
+#         min1 = y[l]
+# for m in range(len(y)):
+#     if y[m] == min1:
+#         continue
+#     z.append(y[m])
+# for o in range(1,len(z)+1):
+#     print(z[-o],end=" ")        
+## min dan boshqa barcha qiymatlarni qaytaruvchi dastur
+# n = int(input("n = "))
+# a = int(input("a = "))
+# min = a
+# x = [a]
+# y = []
+# for i in range(2,n+1):
+#     a = int(input("a = "))
+#     x.append(a)
+#     if min > a:
+#         min = a
+# for j in range(len(x)):
+#     if x[j] == min:
+#         continue
+#     y.append(x[j])
+# for l in range(len(y)):
+#     print(y[l])
+
+# # 24-masala:  
+# n = int(input("n = "))
+# x = []
+# y = []
+# for i in range(1,n+1):
+#     a = int(input("a = "))
+#     x.append(a)
+# for l in range(n):
+#     if l+2 < n:
+#         s = x[l] + x[l + 2]
+#         y.append(s)
+# for j in range(len(y)):
+#     max = y[j]
+#     if max < y[j]:
+#         max = y[j]
+# print(max)
+
+## chala:
+
+# # 25-masala:
+# n = int(input("n = "))
+# x = []
+# y = []
+# for i in range(1,n+1):
+#     a = int(input("a = "))
+#     x.append(a)
+# for j in range(n):
+#     if j + 2 < n:
+#         s = x[j] * x[j+2]
+#         y.append(s)
+# for k in range(len(y)):
+#     min = y[0]
+#     if min > y[k]:
+#         min = y[k]
+# print(min)
+
+# 26-masala:
